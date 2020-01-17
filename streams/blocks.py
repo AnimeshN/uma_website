dcbfa2afb60d321e97aea26bc93980e2f5df9574
AnimeshN <animesh.nauti@gmail.com>
2020-01-17 17:49:58 +0530

    home page integration

--- streams/blocks.py
+++ streams/blocks.py
@@ -1,4 +1,5 @@
 from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
 
 
 class TitleAndTextBlock(blocks.StructBlock):
@@ -37,3 +38,27 @@ class SimpleRichtextBlock(blocks.RichTextBlock):
         template = "streams/richtext_block.html"
         icon = "edit"
         label = "Simple RichText"
+
+
+class CardBlock(blocks.StructBlock):
+    """Cards with image and text and button(s)."""
+
+    title = blocks.CharBlock(required=True, help_text="Add your title")
+
+    cards = blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ("image", ImageChooserBlock(required=True)),
+                ("title", blocks.CharBlock(required=True, max_length=40)),
+                ("text", blocks.TextBlock(required=True, max_length=200)),
+                ("button_page", blocks.PageChooserBlock(required=False)),
+                ("button_url", blocks.URLBlock(required=False, help_text="If the button page above is selected, that will be used first.")),
+            ]
+        )
+    )
+
+    class Meta:  # noqa
+        template = "streams/card_block.html"
+        icon = "placeholder"
+        label = "Add Cards"
+
