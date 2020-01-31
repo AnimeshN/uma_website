5165d069cd7dc7c900c73f30c973635024d79c87
tanishqdayma <tanishqdayma@gmail.com>
2020-01-31 11:56:16 +0530

    Adding GR and RDE resource pages

--- streams/blocks.py
+++ streams/blocks.py
@@ -1,5 +1,6 @@
 from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
+from wagtail.documents.blocks import DocumentChooserBlock
 
 
 class TitleAndTextBlock(blocks.StructBlock):
@@ -62,3 +63,48 @@ class CardBlock(blocks.StructBlock):
         icon = "placeholder"
         label = "Add Cards"
 
+
+# -------------BLOCK FOR GR PAGE--------------
+class ListAndButtonBlock(blocks.StructBlock):
+    """List and buttons of main"""
+
+    title = blocks.CharBlock(required=True)
+
+    points = blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ("point", blocks.CharBlock(required=True, max_length=200)),
+                ("pdfs", blocks.ListBlock(
+                        DocumentChooserBlock(required=True),
+                    )
+                )
+            ]
+        )
+    )
+
+    class Meta:  #noqa
+        template = "streams/list_and_button.html"
+        icon="edit"
+        label="GR Text & Documents"
+
+
+
+# -------------BLOCK FOR RDE PAGE-------------
+class ResourcesBlock(blocks.StructBlock):
+    """List and buttons of main"""
+
+    title = blocks.CharBlock(required=True)
+
+    points = blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ("point", blocks.CharBlock(required=True, max_length=200)),
+                ("url", blocks.URLBlock(required=True)),
+            ]
+        )
+    )
+
+    class Meta:  #noqa
+        template = "streams/resources_page.html"
+        icon="edit"
+        label="Resources"
